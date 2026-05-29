a797b79ffd02f6eab18c864af48b6f7d144d7466
MDTravis <59547361+MDTravisYT@users.noreply.github.com>
2026-05-29 08:38:40 -0400

    Z adjustments

--- Objects/PlaneZ/PLANEZ_PAT.asm
+++ Objects/PlaneZ/PLANEZ_PAT.asm
@@ -1,8 +1,8 @@
-Map_2439:
-Map_2439_0: 	dc.w Map_2439_6-Map_2439
-Map_2439_2: 	dc.w Map_2439_2F-Map_2439
-Map_2439_4: 	dc.w Map_2439_44-Map_2439
-Map_2439_6: 	dc.b $8
+Map_e4e3:
+Map_e4e3_0: 	dc.w Map_e4e3_6-Map_e4e3
+Map_e4e3_2: 	dc.w Map_e4e3_2F-Map_e4e3
+Map_e4e3_4: 	dc.w Map_e4e3_44-Map_e4e3
+Map_e4e3_6: 	dc.b $8
 	dc.b $E8, $F, $80, $0, $E0
 	dc.b $E0, $F, $80, $10, $0
 	dc.b $E0, $0, $80, $20, $20
@@ -11,12 +11,12 @@ Map_2439_6: 	dc.b $8
 	dc.b $0, $F, $80, $28, $0
 	dc.b $10, $9, $80, $38, $E8
 	dc.b $20, $3, $80, $3E, $0
-Map_2439_2F: 	dc.b $4
+Map_e4e3_2F: 	dc.b $4
 	dc.b $E8, $F, $80, $0, $E0
 	dc.b $E0, $F, $80, $10, $0
 	dc.b $E0, $0, $80, $20, $20
 	dc.b $F8, $2, $80, $21, $20
-Map_2439_44: 	dc.b $2
-	dc.b $0, $C, $80, $82, $F0
-	dc.b $0, $0, $80, $86, $10
+Map_e4e3_44: 	dc.b $2
+	dc.b $F8, $C, $80, $82, $E8
+	dc.b $F8, $0, $80, $86, $8
 	even
