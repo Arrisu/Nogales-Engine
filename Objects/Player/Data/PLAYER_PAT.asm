82bd59ea54338d3c90204c99c1ecd0154d119b6b
KatKuriN <69405557+KatKuriN@users.noreply.github.com>
2026-06-04 12:47:50 -0500

    import new assets and credit

--- Objects/Player/Data/PLAYER_PAT.asm
+++ Objects/Player/Data/PLAYER_PAT.asm
@@ -1,95 +1,95 @@
-Map_9e48:
-Map_9e48_0: 	dc.w Map_9e48_2C-Map_9e48
-Map_9e48_2: 	dc.w Map_9e48_2D-Map_9e48
-Map_9e48_4: 	dc.w Map_9e48_47-Map_9e48
-Map_9e48_6: 	dc.w Map_9e48_5C-Map_9e48
-Map_9e48_8: 	dc.w Map_9e48_67-Map_9e48
-Map_9e48_A: 	dc.w Map_9e48_77-Map_9e48
-Map_9e48_C: 	dc.w Map_9e48_8C-Map_9e48
-Map_9e48_E: 	dc.w Map_9e48_9C-Map_9e48
-Map_9e48_10: 	dc.w Map_9e48_AC-Map_9e48
-Map_9e48_12: 	dc.w Map_9e48_BC-Map_9e48
-Map_9e48_14: 	dc.w Map_9e48_CC-Map_9e48
-Map_9e48_16: 	dc.w Map_9e48_DC-Map_9e48
-Map_9e48_18: 	dc.w Map_9e48_EC-Map_9e48
-Map_9e48_1A: 	dc.w Map_9e48_F2-Map_9e48
-Map_9e48_1C: 	dc.w Map_9e48_F8-Map_9e48
-Map_9e48_1E: 	dc.w Map_9e48_FE-Map_9e48
-Map_9e48_20: 	dc.w Map_9e48_104-Map_9e48
-Map_9e48_22: 	dc.w Map_9e48_10A-Map_9e48
-Map_9e48_24: 	dc.w Map_9e48_115-Map_9e48
-Map_9e48_26: 	dc.w Map_9e48_125-Map_9e48
-Map_9e48_28: 	dc.w Map_9e48_12B-Map_9e48
-Map_9e48_2A: 	dc.w Map_9e48_131-Map_9e48
-Map_9e48_2C: 	dc.b $0
-Map_9e48_2D: 	dc.b $5
+Map_e7a7:
+Map_e7a7_0: 	dc.w Map_e7a7_2C-Map_e7a7
+Map_e7a7_2: 	dc.w Map_e7a7_2D-Map_e7a7
+Map_e7a7_4: 	dc.w Map_e7a7_47-Map_e7a7
+Map_e7a7_6: 	dc.w Map_e7a7_5C-Map_e7a7
+Map_e7a7_8: 	dc.w Map_e7a7_67-Map_e7a7
+Map_e7a7_A: 	dc.w Map_e7a7_77-Map_e7a7
+Map_e7a7_C: 	dc.w Map_e7a7_8C-Map_e7a7
+Map_e7a7_E: 	dc.w Map_e7a7_9C-Map_e7a7
+Map_e7a7_10: 	dc.w Map_e7a7_AC-Map_e7a7
+Map_e7a7_12: 	dc.w Map_e7a7_BC-Map_e7a7
+Map_e7a7_14: 	dc.w Map_e7a7_CC-Map_e7a7
+Map_e7a7_16: 	dc.w Map_e7a7_DC-Map_e7a7
+Map_e7a7_18: 	dc.w Map_e7a7_EC-Map_e7a7
+Map_e7a7_1A: 	dc.w Map_e7a7_F2-Map_e7a7
+Map_e7a7_1C: 	dc.w Map_e7a7_F8-Map_e7a7
+Map_e7a7_1E: 	dc.w Map_e7a7_FE-Map_e7a7
+Map_e7a7_20: 	dc.w Map_e7a7_104-Map_e7a7
+Map_e7a7_22: 	dc.w Map_e7a7_10A-Map_e7a7
+Map_e7a7_24: 	dc.w Map_e7a7_115-Map_e7a7
+Map_e7a7_26: 	dc.w Map_e7a7_125-Map_e7a7
+Map_e7a7_28: 	dc.w Map_e7a7_12B-Map_e7a7
+Map_e7a7_2A: 	dc.w Map_e7a7_131-Map_e7a7
+Map_e7a7_2C: 	dc.b $0
+Map_e7a7_2D: 	dc.b $5
 	dc.b $EC, $8, $0, $0, $F0
 	dc.b $F4, $D, $0, $3, $F0
 	dc.b $4, $8, $0, $B, $F0
 	dc.b $C, $8, $0, $E, $F0
 	dc.b $4, $1, $0, $11, $8
-Map_9e48_47: 	dc.b $4
+Map_e7a7_47: 	dc.b $4
 	dc.b $EB, $D, $0, $0, $EC
 	dc.b $FB, $9, $0, $8, $EC
 	dc.b $FB, $6, $0, $E, $4
 	dc.b $B, $4, $0, $14, $EC
-Map_9e48_5C: 	dc.b $2
+Map_e7a7_5C: 	dc.b $2
 	dc.b $EC, $D, $0, $0, $EC
 	dc.b $FC, $E, $0, $8, $F3
-Map_9e48_67: 	dc.b $3
+Map_e7a7_67: 	dc.b $3
 	dc.b $ED, $9, $0, $0, $F4
 	dc.b $FD, $A, $0, $6, $F4
 	dc.b $FC, $0, $0, $10, $C
-Map_9e48_77: 	dc.b $4
+Map_e7a7_77: 	dc.b $4
 	dc.b $EB, $9, $0, $0, $F4
 	dc.b $FB, $9, $0, $6, $EC
 	dc.b $FB, $6, $0, $C, $4
 	dc.b $B, $4, $0, $12, $EC
-Map_9e48_8C: 	dc.b $3
+Map_e7a7_8C: 	dc.b $3
 	dc.b $EC, $9, $0, $0, $F4
 	dc.b $FC, $E, $0, $6, $EC
 	dc.b $FB, $0, $0, $13, $C
-Map_9e48_9C: 	dc.b $3
+Map_e7a7_9C: 	dc.b $3
 	dc.b $ED, $D, $0, $0, $EC
 	dc.b $FD, $C, $0, $8, $F4
 	dc.b $5, $9, $0, $C, $F4
-Map_9e48_AC: 	dc.b $3
+Map_e7a7_AC: 	dc.b $3
 	dc.b $EE, $9, $0, $0, $F4
 	dc.b $FE, $E, $0, $6, $EC
 	dc.b $FD, $0, $0, $13, $C
-Map_9e48_BC: 	dc.b $3
+Map_e7a7_BC: 	dc.b $3
 	dc.b $EE, $9, $0, $0, $F4
 	dc.b $FE, $E, $0, $6, $EC
 	dc.b $FD, $0, $0, $13, $C
-Map_9e48_CC: 	dc.b $3
+Map_e7a7_CC: 	dc.b $3
 	dc.b $EE, $9, $0, $0, $F4
 	dc.b $FE, $E, $0, $6, $EC
 	dc.b $FD, $0, $0, $13, $C
-Map_9e48_DC: 	dc.b $3
+Map_e7a7_DC: 	dc.b $3
 	dc.b $EE, $9, $0, $0, $F4
 	dc.b $FE, $E, $0, $6, $EC
 	dc.b $FD, $0, $0, $13, $C
-Map_9e48_EC: 	dc.b $1
+Map_e7a7_EC: 	dc.b $1
 	dc.b $F0, $F, $0, $0, $F0
-Map_9e48_F2: 	dc.b $1
+Map_e7a7_F2: 	dc.b $1
 	dc.b $F0, $F, $0, $0, $F0
-Map_9e48_F8: 	dc.b $1
+Map_e7a7_F8: 	dc.b $1
 	dc.b $F0, $F, $0, $0, $F0
-Map_9e48_FE: 	dc.b $1
+Map_e7a7_FE: 	dc.b $1
 	dc.b $F0, $F, $0, $0, $F0
-Map_9e48_104: 	dc.b $1
+Map_e7a7_104: 	dc.b $1
 	dc.b $F0, $F, $0, $0, $F0
-Map_9e48_10A: 	dc.b $2
+Map_e7a7_10A: 	dc.b $2
 	dc.b $EC, $8, $0, $0, $F0
 	dc.b $F4, $F, $0, $3, $F0
-Map_9e48_115: 	dc.b $3
+Map_e7a7_115: 	dc.b $3
 	dc.b $EC, $8, $0, $0, $F0
 	dc.b $F4, $E, $0, $3, $F0
 	dc.b $C, $8, $0, $F, $F8
-Map_9e48_125: 	dc.b $1
+Map_e7a7_125: 	dc.b $1
 	dc.b $F0, $B, $0, $0, $F4
-Map_9e48_12B: 	dc.b $1
+Map_e7a7_12B: 	dc.b $1
 	dc.b $F4, $6, $0, $0, $F8
-Map_9e48_131: 	dc.b $1
+Map_e7a7_131: 	dc.b $1
 	dc.b $F8, $1, $0, $0, $FC
 	even
--- README.md
+++ README.md
@@ -6,8 +6,9 @@ This is not gunning for 100% accuracy, but rather for "technical" accuracy
 
 ## Credits 
 - Emma (Character sprites, Title sprites, Palmtrees, Mountains GFX)
+- Louplayer (Near-perfect screenshot replicas, support and adjustments)
+- MDTravis (Initial imports, layout help, general research)
 - Leatheraxe (Chunks and playfield GFX help)
-- Louplayer (Mountains GFX)
 - Scraps (Chunk reference)
 - Icetroids (Original base assets)
-- Project HI-TECH '90 (Updated Z-Rock gfx)
+- Project HI-TECH '90 (Referenced Z-Rock gfx)
