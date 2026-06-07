7b670d60a82895d359d376f034adfa4a4231dc27
MDTravis <59547361+MDTravisYT@users.noreply.github.com>
2026-06-06 22:32:06 -0400

    Implement grass overlay object

---
+++ Objects/Grass/GRASS_PAT.asm
@@ -0,0 +1,14 @@
+Map_7031:
+Map_7031_0: 	dc.w Map_7031_4-Map_7031
+Map_7031_2: 	dc.w Map_7031_1E-Map_7031
+Map_7031_4: 	dc.b $5
+	dc.b $FC, $4, $8, $43, $D8
+	dc.b $FC, $4, $0, $43, $E8
+	dc.b $FC, $4, $8, $43, $F8
+	dc.b $FC, $4, $8, $43, $8
+	dc.b $FC, $4, $0, $43, $18
+Map_7031_1E: 	dc.b $3
+	dc.b $FC, $C, $0, $B8, $D8
+	dc.b $FC, $C, $0, $BC, $F8
+	dc.b $FC, $4, $0, $C0, $18
+	even
