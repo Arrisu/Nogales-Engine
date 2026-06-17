90d074b942687f55bd28613cdaadfee510472391
KatKuriN <69405557+KatKuriN@users.noreply.github.com>
2026-06-17 13:06:19 -0500

    some adjustments to its pattern data

--- Objects/Enemy/ENEMY_PAT.asm
+++ Objects/Enemy/ENEMY_PAT.asm
@@ -1,20 +1,20 @@
-Map_9acf:
-Map_9acf_0: 	dc.w Map_9acf_A-Map_9acf
-Map_9acf_2: 	dc.w Map_9acf_B-Map_9acf
-Map_9acf_4: 	dc.w Map_9acf_16-Map_9acf
-Map_9acf_6: 	dc.w Map_9acf_21-Map_9acf
-Map_9acf_8: 	dc.w Map_9acf_2C-Map_9acf
-Map_9acf_A: 	dc.b $0
-Map_9acf_B: 	dc.b $2
-	dc.b $F2, $B, $0, $0, $F3
-	dc.b $EA, $8, $0, $C, $F3
-Map_9acf_16: 	dc.b $2
+Map_e36f:
+Map_e36f_0: 	dc.w Map_e36f_A-Map_e36f
+Map_e36f_2: 	dc.w Map_e36f_B-Map_e36f
+Map_e36f_4: 	dc.w Map_e36f_16-Map_e36f
+Map_e36f_6: 	dc.w Map_e36f_21-Map_e36f
+Map_e36f_8: 	dc.w Map_e36f_2C-Map_e36f
+Map_e36f_A: 	dc.b $0
+Map_e36f_B: 	dc.b $2
+	dc.b $F2, $B, $0, $0, $F4
+	dc.b $EA, $8, $0, $C, $F4
+Map_e36f_16: 	dc.b $2
 	dc.b $EA, $C, $0, $F, $F3
 	dc.b $F2, $F, $0, $13, $F3
-Map_9acf_21: 	dc.b $2
-	dc.b $EC, $F, $0, $23, $F3
-	dc.b $B, $4, $0, $33, $FB
-Map_9acf_2C: 	dc.b $2
-	dc.b $F0, $F, $0, $35, $F0
-	dc.b $F0, $1, $0, $45, $10
+Map_e36f_21: 	dc.b $2
+	dc.b $EC, $F, $0, $23, $F1
+	dc.b $B, $4, $0, $33, $F9
+Map_e36f_2C: 	dc.b $2
+	dc.b $F1, $F, $0, $35, $F3
+	dc.b $F1, $1, $0, $45, $13
 	even
