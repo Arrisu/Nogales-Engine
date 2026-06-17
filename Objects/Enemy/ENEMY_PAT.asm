e326f84247582f5214d52c0fd54cd0163e623e42
KatKuriN <69405557+KatKuriN@users.noreply.github.com>
2026-06-17 12:37:16 -0500

    testing

--- Objects/Enemy/ENEMY_PAT.asm
+++ Objects/Enemy/ENEMY_PAT.asm
@@ -1,20 +1,20 @@
-Map_5bc6:
-Map_5bc6_0: 	dc.w Map_5bc6_A-Map_5bc6
-Map_5bc6_2: 	dc.w Map_5bc6_B-Map_5bc6
-Map_5bc6_4: 	dc.w Map_5bc6_16-Map_5bc6
-Map_5bc6_6: 	dc.w Map_5bc6_21-Map_5bc6
-Map_5bc6_8: 	dc.w Map_5bc6_2C-Map_5bc6
-Map_5bc6_A: 	dc.b $0
-Map_5bc6_B: 	dc.b $2
+Map_9acf:
+Map_9acf_0: 	dc.w Map_9acf_A-Map_9acf
+Map_9acf_2: 	dc.w Map_9acf_B-Map_9acf
+Map_9acf_4: 	dc.w Map_9acf_16-Map_9acf
+Map_9acf_6: 	dc.w Map_9acf_21-Map_9acf
+Map_9acf_8: 	dc.w Map_9acf_2C-Map_9acf
+Map_9acf_A: 	dc.b $0
+Map_9acf_B: 	dc.b $2
 	dc.b $F2, $B, $0, $0, $F3
 	dc.b $EA, $8, $0, $C, $F3
-Map_5bc6_16: 	dc.b $2
+Map_9acf_16: 	dc.b $2
 	dc.b $EA, $C, $0, $F, $F3
 	dc.b $F2, $F, $0, $13, $F3
-Map_5bc6_21: 	dc.b $2
+Map_9acf_21: 	dc.b $2
 	dc.b $EC, $F, $0, $23, $F3
 	dc.b $B, $4, $0, $33, $FB
-Map_5bc6_2C: 	dc.b $2
+Map_9acf_2C: 	dc.b $2
 	dc.b $F0, $F, $0, $35, $F0
 	dc.b $F0, $1, $0, $45, $10
 	even
