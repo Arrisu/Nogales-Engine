2be0cb95e20dac8c6151703dc1480aecdcc5c371
KatKuriN <69405557+KatKuriN@users.noreply.github.com>
2026-06-17 13:24:07 -0500

    temporary backwards jump

--- Objects/Enemy/ENEMY_PAT.asm
+++ Objects/Enemy/ENEMY_PAT.asm
@@ -1,20 +1,20 @@
-Map_e36f:
-Map_e36f_0: 	dc.w Map_e36f_A-Map_e36f
-Map_e36f_2: 	dc.w Map_e36f_B-Map_e36f
-Map_e36f_4: 	dc.w Map_e36f_16-Map_e36f
-Map_e36f_6: 	dc.w Map_e36f_21-Map_e36f
-Map_e36f_8: 	dc.w Map_e36f_2C-Map_e36f
-Map_e36f_A: 	dc.b $0
-Map_e36f_B: 	dc.b $2
+Map_6f58:
+Map_6f58_0: 	dc.w Map_6f58_A-Map_6f58
+Map_6f58_2: 	dc.w Map_6f58_B-Map_6f58
+Map_6f58_4: 	dc.w Map_6f58_16-Map_6f58
+Map_6f58_6: 	dc.w Map_6f58_21-Map_6f58
+Map_6f58_8: 	dc.w Map_6f58_2C-Map_6f58
+Map_6f58_A: 	dc.b $0
+Map_6f58_B: 	dc.b $2
 	dc.b $F2, $B, $0, $0, $F4
 	dc.b $EA, $8, $0, $C, $F4
-Map_e36f_16: 	dc.b $2
+Map_6f58_16: 	dc.b $2
 	dc.b $EA, $C, $0, $F, $F3
 	dc.b $F2, $F, $0, $13, $F3
-Map_e36f_21: 	dc.b $2
-	dc.b $EC, $F, $0, $23, $F1
-	dc.b $B, $4, $0, $33, $F9
-Map_e36f_2C: 	dc.b $2
-	dc.b $F1, $F, $0, $35, $F3
-	dc.b $F1, $1, $0, $45, $13
+Map_6f58_21: 	dc.b $2
+	dc.b $EB, $F, $0, $23, $F0
+	dc.b $B, $4, $0, $33, $F8
+Map_6f58_2C: 	dc.b $2
+	dc.b $F0, $F, $0, $35, $F0
+	dc.b $F0, $1, $0, $45, $10
 	even
