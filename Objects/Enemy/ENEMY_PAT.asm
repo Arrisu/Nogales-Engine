da7bff66c96d50338dea11d069e392b3069d1b3f
KatKuriN <69405557+KatKuriN@users.noreply.github.com>
2026-06-19 11:02:42 -0500

    lo

--- Objects/Enemy/ENEMY_PAT.asm
+++ Objects/Enemy/ENEMY_PAT.asm
@@ -1,20 +1,20 @@
-Map_6f58:
-Map_6f58_0: 	dc.w Map_6f58_A-Map_6f58
-Map_6f58_2: 	dc.w Map_6f58_B-Map_6f58
-Map_6f58_4: 	dc.w Map_6f58_16-Map_6f58
-Map_6f58_6: 	dc.w Map_6f58_21-Map_6f58
-Map_6f58_8: 	dc.w Map_6f58_2C-Map_6f58
-Map_6f58_A: 	dc.b $0
-Map_6f58_B: 	dc.b $2
-	dc.b $F2, $B, $0, $0, $F4
-	dc.b $EA, $8, $0, $C, $F4
-Map_6f58_16: 	dc.b $2
-	dc.b $EA, $C, $0, $F, $F3
-	dc.b $F2, $F, $0, $13, $F3
-Map_6f58_21: 	dc.b $2
-	dc.b $EB, $F, $0, $23, $F0
-	dc.b $B, $4, $0, $33, $F8
-Map_6f58_2C: 	dc.b $2
+Map_fb31:
+Map_fb31_0: 	dc.w Map_fb31_A-Map_fb31
+Map_fb31_2: 	dc.w Map_fb31_B-Map_fb31
+Map_fb31_4: 	dc.w Map_fb31_16-Map_fb31
+Map_fb31_6: 	dc.w Map_fb31_21-Map_fb31
+Map_fb31_8: 	dc.w Map_fb31_2C-Map_fb31
+Map_fb31_A: 	dc.b $0
+Map_fb31_B: 	dc.b $2
+	dc.b $F4, $B, $0, $0, $F4
+	dc.b $EC, $8, $0, $C, $F4
+Map_fb31_16: 	dc.b $2
+	dc.b $EC, $C, $0, $F, $F3
+	dc.b $F4, $F, $0, $13, $F3
+Map_fb31_21: 	dc.b $2
+	dc.b $F0, $F, $0, $23, $F0
+	dc.b $10, $4, $0, $33, $F8
+Map_fb31_2C: 	dc.b $2
 	dc.b $F0, $F, $0, $35, $F0
 	dc.b $F0, $1, $0, $45, $10
 	even
